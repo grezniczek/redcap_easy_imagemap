d485252e16c70df694cebc4a7ff31e1786c9eb42
Günther Rezniczek <g.rezniczek@gmail.com>
2022-07-13 14:58:40 +0200

    Browser side display logic - basics

---
+++ js/EasyImagemap-Display.js
@@ -0,0 +1,103 @@
+// Easy Imagemap EM
+// @ts-check
+;(function() {
+
+    // @ts-ignore
+    const EIM = window.DE_RUB_EasyImagemap ?? {
+        init: initialize
+    };
+    // @ts-ignore
+    window.DE_RUB_EasyImagemap = EIM;
+
+    var config = {};
+
+    function initialize(config_data) {
+        config = config_data;
+        log('Initialzed', config);
+    }
+
+
+    
+
+
+//#region -- Debug Logging
+
+/**
+ * Logs a message to the console when in debug mode
+ */
+ function log() {
+    if (!config.debug) return;
+    var ln = '??';
+    try {
+        var line = ((new Error).stack ?? '').split('\n')[2];
+        var parts = line.split(':');
+        ln = parts[parts.length - 2];
+    }
+    catch(err) { }
+    log_print(ln, 'log', arguments);
+}
+/**
+ * Logs a warning to the console when in debug mode
+ */
+function warn() {
+    if (!config.debug) return;
+    var ln = '??';
+    try {
+        var line = ((new Error).stack ?? '').split('\n')[2];
+        var parts = line.split(':');
+        ln = parts[parts.length - 2];
+    }
+    catch(err) { }
+    log_print(ln, 'warn', arguments);
+}
+
+/**
+ * Logs an error to the console when in debug mode
+ */
+function error() {
+    var ln = '??';
+    try {
+        var line = ((new Error).stack ?? '').split('\n')[2];
+        var parts = line.split(':');
+        ln = parts[parts.length - 2];
+    }
+    catch(err) { }
+    log_print(ln, 'error', arguments);;
+}
+
+/**
+ * Prints to the console
+ * @param {string} ln Line number where log was called from
+ * @param {'log'|'warn'|'error'} mode
+ * @param {IArguments} args
+ */
+function log_print(ln, mode, args) {
+    var prompt = 'EasyImagemap v' + config.version + ' [' + ln + ']';
+    switch(args.length) {
+        case 1:
+            console[mode](prompt, args[0]);
+            break;
+        case 2:
+            console[mode](prompt, args[0], args[1]);
+            break;
+        case 3:
+            console[mode](prompt, args[0], args[1], args[2]);
+            break;
+        case 4:
+            console[mode](prompt, args[0], args[1], args[2], args[3]);
+            break;
+        case 5:
+            console[mode](prompt, args[0], args[1], args[2], args[3], args[4]);
+            break;
+        case 6:
+            console[mode](prompt, args[0], args[1], args[2], args[3], args[4], args[5]);
+            break;
+        default:
+            console[mode](prompt, args);
+            break;
+    }
+}
+
+//#endregion
+
+})();
